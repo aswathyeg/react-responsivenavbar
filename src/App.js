b5b4b9e0c518fbb0c7c3e15c0f4fa74af8bdf910
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2023-12-01 17:10:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-
+// Filename - App.js
 
 import React from "react";
-import {FooterContainer} from './containers/footer';
-function App() {        
-  return (
-    <>
-      <FooterContainer/>
-     
-    </>
-  );
+// import "./App.css";
+import Footer from "./components/Footer.js";
+function App() {
+	return (
+		<div>
+			<div
+				style={{
+					minHeight: "400px",
+					color: "green",
+				}}
+			>
+				<h1>Footer</h1>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
