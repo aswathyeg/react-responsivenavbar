a2d78859de55d123135734bc017cd953022703f8
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-04-02 15:31:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import React from "react";
 // import "./App.css";
 import Footer from "./components/Footer.js";
 function App() {
-	return (
-		<div>
-			<div
-				style={{
-					minHeight: "400px",
-					color: "green",
-				}}
-			>
-				<h1>Footer</h1>
-				
-			</div>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <div
+        style={{
+          minHeight: "400px",
+          color: "green",
+        }}
+      >
+        <h1>Footer</h1>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
